7961428d53fa3a5b0e0a06f1c9c36b8d828ee62a
Dawid <273202@student.pwr.edu.pl>
2025-11-10 00:58:09 +0100

    zadanie 4.5 test ID 273202@student.pwr.edu.pl

--- src/test/java/TestClientRequest.java
+++ src/test/java/TestClientRequest.java
@@ -12,9 +12,30 @@ import java.util.stream.IntStream;
 public class TestClientRequest {
 
     private final Fixture fixture = new Fixture();
+    private clientRequest.Customer randomCustomer;
+    private List<clientRequest.Product> randomProducts;
+
+    TestClientRequest() {
+        randomCustomer = new clientRequest.Customer(
+                generateRandomString(6), // firstName
+                generateRandomString(6), // lastName
+                generateRandomString(10), // email
+                generateRandomString(9)  // phoneNumber
+        );
+
+        int productCount = (int) (Math.random() * 12);
+        randomProducts = IntStream.range(0, productCount)
+                .mapToObj(i -> new clientRequest.Product(
+                        generateRandomString(3),            // productCode
+                        Math.abs(fixture.create(Integer.class)) % 100 + 1, // quantity >= 1
+                        randomUnit()             // unit
+                ))
+                .collect(Collectors.toList());
+        clientRequest testRequest = new clientRequest(randomCustomer, randomProducts);
+    }
 
     private String generateRandomString(int maxLength) {
-        String temp = fixture.create(String.class);
+        String temp = fixture.create(String.class).replaceAll("-", "");
         if(temp.length()>maxLength){
             return temp.substring(0, maxLength);
         }
@@ -26,26 +47,16 @@ public class TestClientRequest {
 
     @Test
     void testValidRandomOrder() {
-        clientRequest.Customer randomCustomer = new clientRequest.Customer(
-                generateRandomString(6), // firstName
-                generateRandomString(6), // lastName
-                generateRandomString(10), // email
-                generateRandomString(9)  // phoneNumber
-        );
-
-        int productCount = (int)(Math.random() * 12);
-        List<clientRequest.Product> randomProducts = IntStream.range(0, productCount)
-                .mapToObj(i -> new clientRequest.Product(
-                        generateRandomString(3),            // productCode
-                        Math.abs(fixture.create(Integer.class)) % 100 + 1, // quantity >= 1
-                        randomUnit()             // unit
-                ))
-                .collect(Collectors.toList());
-
-
         clientRequest testRequest = new clientRequest(randomCustomer, randomProducts);
         System.out.println("Order ID: " + testRequest.getOrderID());
         assertDoesNotThrow(() -> testRequest.validate());
     }
 
+    @Test
+    void testValidateOrderID(){
+        clientRequest testRequest = new clientRequest(randomCustomer, randomProducts);
+        long correctSymbolCount = 3 + (3 * testRequest.getProducts().size());
+        long actualSymbolCount = testRequest.getOrderID().chars().filter(ch -> ch == '-').count();
+        assertEquals(correctSymbolCount, actualSymbolCount);
+    }
 }
